e920e5745407b844bc086ffb6d764d9618e08517
hurdedur <diti401@hotmail.com>
2026-03-02 20:34:40 -0500

    Updated Card.js
    updated Card.js to have the card value and suit displayed in the center and corner

---
+++ Card.js
@@ -0,0 +1,13 @@
+function Card({value, suit, picked, onClick}) {
+    const red = suit === '♥' || suit === '♦';
+
+    return (
+        <div className={`card ${red ? "red" : ""} ${picked ? "picked" : ""}`} onClick={onClick}>
+            <div className="corner">{value}{suit}</div>
+            <div className="center">{suit}</div>
+            <div className="corner bottom">{value}{suit}</div>
+        </div>
+    )
+}
+
+export default Card;
